1af04fffdb5987b09044cb8dd1b1ccf90b46ccab
David Gaynor <runnedrun@gmail.com>
2016-11-06 15:47:50 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ goForward(100)
 turnRight()
 goForward(100)
 turnLeft()
-
-if (false){
-  console.log("is this running?")
 goForward(100)
 turnLeft()
 goForward(100)
 turnRight()
+
+var object = {
+  
 }
 
 var name = "Q"
 console.log(name)
+
